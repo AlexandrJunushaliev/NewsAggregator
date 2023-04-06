e983e9e3e9a148c0bcd34ed416b058987650bd07
Alexandr Dzhunushaliev <dzhunall@mail.ru>
2023-04-06 23:36:51 +0300

    feat: fst SearchIndexEntryId

---
+++ NewsProcessor/Domain/SearchIndexEntryId.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.Serialization;
+
+namespace NewsProcessor.Domain;
+
+[DataContract]
+public readonly struct SearchIndexEntryId : IComparable
+{
+    [DataMember]
+    public readonly int Id;
+
+    [DataMember]
+    public readonly DateTime DateTime;
+
+    public SearchIndexEntryId(int id, DateTime dateTime)
+    {
+        Id = id;
+        DateTime = dateTime;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not SearchIndexEntryId id)
+        {
+            throw new ArgumentException($"Wrong parameter type {obj}");
+        }
+
+        return id.Id == Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    public int CompareTo(object? obj)
+    {
+        if (obj is not SearchIndexEntryId id)
+        {
+            throw new ArgumentException($"Wrong parameter type {obj}");
+        }
+
+        var cmp = DateTime.CompareTo(id.DateTime);
+        if (cmp == 0)
+            return Id.CompareTo(id.Id);
+        return cmp;
+    }
+}
